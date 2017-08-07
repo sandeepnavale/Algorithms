905cbeff9863c284137f4080869161fd78b7d2a5
sandeepnavale <mail2sandeepnl@gmail.com>
2017-08-07 20:23:29 +0530

    Version 0

---
+++ LinkedLists/test_singlyLinkedList.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestSinglyLinkedList(TestCase):
+    def test_append(self):
+        from LinkedLists import SinglyLinkedList
+        self.assertTrue(append(1))
