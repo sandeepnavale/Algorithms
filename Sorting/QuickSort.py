28f4fe8b1bc1841631bb21a2448226dc80e66321
sandeepnavale <mail2sandeepnl@gmail.com>
2017-12-08 10:42:49 +0530

    Added many programs

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,26 +1,22 @@
-def quicksort(nums, start=0, end=None):
-    """Quicksort using last element as pivot."""
-    def partition(nums, start, end):
-        pindex = start
-        pivot = end
-        for i in range(start, end):
-            if nums[i] <= nums[pivot]:
-                nums[i], nums[pindex] = nums[pindex], nums[i]
-                pindex += 1
-        nums[pindex], nums[pivot] = nums[pivot], nums[pindex]
-        return pindex
+from random import randint
+def quicksort(seq):
+    if len(seq) <= 1: return seq # Base case
+    lo, pi, hi = partition(seq) # pi is in its place
+    return quicksort(lo) + [pi] + quicksort(hi) # Sort lo and hi separately
 
-    if not end:
-        end = len(nums) - 1
-    if start >= end:
-        return None
-    pivot = partition(nums, start, end)
-    quicksort(nums, start, pivot - 1)
-    quicksort(nums, pivot + 1, end)
+def partition(seq):
+    pi, seq = seq[0], seq[1:]
+    lo = [x for x in seq if x <= pi]
+    hi = [x for x in seq if x > pi]
+    return lo, pi, hi
+    # Pick and remove the pivot # All the small elements
+    # All the large ones
+    # pi is "in the right place"
 
+print (quicksort([10,22,3,445,6,1,100]))
+#print (quicksort([111,22,43,455645,6,1,1400]))
+print (quicksort(["","sandeep","nls","abc","az",'a']))
 
-
-a = [2, 9, 2, 3, 5, 8, 1]
-quicksort(a)
-assert a == [1, 2, 2, 3, 5, 8, 9]
-print('All passed!')
+a = [randint(10,100) for i in range(10)]
+print("Input List ",a)
+print("Sorted List ",quicksort(a))
