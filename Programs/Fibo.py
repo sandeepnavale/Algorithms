a7b926598ce3430388871461a76c820492ecc7bc
sandeepnavale <mail2sandeepnl@gmail.com>
2017-12-23 18:02:07 +0530

    Added fibo recursive method

--- Programs/Fibo.py
+++ Programs/Fibo.py
@@ -7,3 +7,15 @@ def fibo(n):
 
 
 fibo(1000)
+
+# Generate n number of fibo numbers
+knownNum = {0:0,1:1}
+def fib_rec(num):
+    if num in knownNum:
+        return knownNum[num]
+    fibnum = fib_rec(num-1) + fib_rec(num-2)
+    knownNum[num]= fibnum
+    return fibnum
+
+fib_rec(10)
+print(knownNum)
