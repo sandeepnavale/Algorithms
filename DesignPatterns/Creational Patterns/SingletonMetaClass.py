4efb83252a34657288b7e297336e6582e7163bbd
sandeepnl <sandeepnl@outlook.com>
2019-07-10 11:46:25 +1000

    Modified Singleton class.

--- DesignPatterns/Creational Patterns/SingletonMetaClass.py
+++ DesignPatterns/Creational Patterns/SingletonMetaClass.py
@@ -7,22 +7,23 @@ class Singleton(type):
         return cls.instance
 
 
-class ASingleton(object):
-    __metaclass__ = Singleton
-
+class ASingleton(metaclass = Singleton):
+    # __metaclass__ = Singleton  THIS don't work in python 
+    pass 
 
 a = ASingleton()
 b = ASingleton()
-import ipdb; ipdb.set_trace()
 
 print(a is b)
 print(a.__class__.__name__, b.__class__.__name__)
 
-class BSingleton(object):
-    __metaclass__ = Singleton
+class BSingleton(metaclass = Singleton):
+    # __metaclass__ = Singleton this don;t work in Python 3
+    pass
 
 c = BSingleton()
 d = BSingleton()
 
 print(c.__class__.__name__, d.__class__.__name__)
+print(id(a)==id(b),id(c)==id(d))
 print(id(a),id(b),id(c),id(d))
